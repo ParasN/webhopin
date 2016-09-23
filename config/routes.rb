357f446992ab84e1e14f1cbb5a1537795916b20e
Mohak Juneja <mohakjuneja@yahoo.com>
2016-09-23 13:38:00 +0530

    modify header

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,15 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
 
-  get 'static_pages/about'
+  # get 'static_pages/about'
 
-  get 'static_pages/contact'
+  # get 'static_pages/contact'
 
-  get 'static_pages/policy'
+  # get 'static_pages/policy'
+
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/policy', to: 'static_pages#policy'      
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
